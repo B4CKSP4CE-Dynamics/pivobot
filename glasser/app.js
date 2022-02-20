e8ba81c2a41fdeb61948fb0339d991a5511bdc6c
aanper <mail@s3f.ru>
2022-02-20 23:53:07 +0300

    refactor code structure

--- glasser/app.js
+++ glasser/app.js
@@ -6,12 +6,7 @@ function update(dt, t) {
 
 }
 
-function app(pixi) {
-    let scene = new PIXI.Container();
-    pixi.stage = scene;
-    scene.interactive = true;
-    scene.cursor = 'pointer';
-
+function setup(pixi, scene) {
     let background = new PIXI.Graphics()
         .beginFill(0x444444)
         .drawRect(0, 0, pixi.screen.width, pixi.screen.height)
@@ -45,26 +40,4 @@ function app(pixi) {
     const base = world.addBox2d(PIXI.box2d.Rectangle.from(new PIXI.Graphics().beginFill(0x0000FF).drawRect(0, 0, 450 / 2, baseHeight), { isStatic: true }));
     base.setX(450 / 2 - base.width / 2);
     base.setY(800 - baseHeight - 100);
-
-    window.addEventListener(
-        "keydown",
-        (event) => {
-            key_handler(event.keyCode, true);
-            if( event.keyCode == 38 || event.keyCode == 40 || event.keyCode == 13 /*event.keyCode !== 116 && event.keyCode !== 122 && event.keyCode !== 123*/) {
-                event.preventDefault();
-            }
-        },
-        false
-    );
-
-    window.addEventListener(
-        "keyup",
-        (event) => {
-            key_handler(event.keyCode, false);
-            event.preventDefault();
-        },
-        false
-    );
-
-    pixi.ticker.add(delta => update(delta, performance.now()));
 }
